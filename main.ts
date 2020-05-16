0dc51f5f4f459d0d5459c95998fcdd3bfd96d317
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-05-16 16:37:06 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -4,9 +4,12 @@
 //% color=#006464 weight=20 icon="\uf1b9" block="softServo"
 namespace softservo {
 
+    let initFlag = 0;
+    let PinNo: number;
+    let PwmValue: number;
     const interval = 20000;
     let nextTime: number;
-    let initFlag = 0;
+
 
     /**
      * softServo
@@ -14,26 +17,30 @@ namespace softservo {
      * @param value degree, eg: 90
      */
     //% blockId="softServo" block="soft Servo %pin %degree"
-    export function softServo(pin: DigitalPin,degree: number): void {
+    export function softServo(pin: DigitalPin, degree: number): void {
 
-        let PinNo = pin;
-        let PwmValue = Math.round(degree * 1000 / 90) + 500;
+        PinNo = pin;
+        PwmValue = Math.round(degree * 1000 / 90) + 500;
 
         if (initFlag == 0) {
-            initFlag=1
-            nextTime=input.runningTimeMicros();
-
-            control.inBackground(function () {
-                while (true) {
-                    while (input.runningTimeMicros() < nextTime) {
-                        basic.pause(2)
-                    }
-                    nextTime += interval;
-                    pins.digitalWritePin(PinNo, 1)
-                    control.waitMicros(PwmValue)
-                    pins.digitalWritePin(PinNo, 0)
-                }
-            })
+            loop();
+            initFlag = 1;
         }
+    }
+    function loop() {
+
+        let nextTime = input.runningTimeMicros();
+        control.inBackground(function () {
+            while (true) {
+                while (input.runningTimeMicros() < nextTime) {
+                    basic.pause(2)
+                }
+                nextTime += interval;
+                pins.digitalWritePin(PinNo, 1)
+                control.waitMicros(PwmValue)
+                pins.digitalWritePin(PinNo, 0)
+                nextTime += interval;
+            }
+        })
     }
 }
--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 basic.forever(function () {
-    for(let d=0;d<=180;d+=10){
+    for (let d = 0; d <= 180; d += 10) {
         softservo.softServo(DigitalPin.P1, d)
+        watchfont.showSorobanNumber(d, 0, 5)
         basic.pause(1000)
     }
 })
