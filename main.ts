9d8ce4c7a9f68cb1a34e14460fee5a22b539fba5
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-05-16 11:58:01 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,68 +1,39 @@
 /*
-  softPWM
+  softServo
 */
-//% color=#006464 weight=20 icon="\uf1b9" block="softPWM"
-enum pwmMode {
-    //%block=servo
-    servo = 0,
-    //%block=analog
-    analog = 1
-}
-namespace softPWM {
+//% color=#006464 weight=20 icon="\uf1b9" block="softServo"
+namespace softservo {
 
-    let PwmMode: pwmMode;
-    let PinNo: DigitalPin;
-    let Interval: number;
-    let InitFlag = 0;
-    let StartTime: number, NextTime: number;
-    let PwmValue = 0;
+    const interval = 20000;
+    let nextTime: number;
+    let initFlag = 0;
 
     /**
-     * PWM in software
-     * @param mode PWM mode , eg: pwmMode.servo
-     * @param interval pwm Interval in uS, eg: 20000
-     */
-    //% blockId="initpwm" block="initPWM %mode %interval"
-    export function initPwm(mode: pwmMode,  interval: number): void {
-        StartTime = input.runningTimeMicros();
-        NextTime = StartTime;
-        PwmMode = mode;
-        Interval = interval;
-    }
-
-    /**
-     * start PWM
+     * softServo
      * @param pin Output pin number , eg: DigitalPin.P8
-     * @param value PWM value, eg: 90
+     * @param value degree, eg: 90
      */
-    //% blockId="setPwmValue" block="set PWM value %pin %value"
-    export function setPwmValue(pin: DigitalPin,value: number): void {
-        PinNo = pin;
-        PwmValue = value;
-        if (InitFlag == 0) {
-            loop();
+    //% blockId="softServo" block="soft Servo %pin %degree"
+    export function softServo(pin: DigitalPin,degree: number): void {
+
+        let PinNo = pin;
+        let PwmValue = Math.round(degree * 1000 / 90) + 500;
+
+        if (initFlag == 0) {
+            initFlag=1
+            nextTime=input.runningTimeMicros();
+
+            control.inBackground(function () {
+                while (true) {
+                    while (input.runningTimeMicros() < nextTime) {
+                        basic.pause(2)
+                    }
+                    nextTime += interval;
+                    pins.digitalWritePin(PinNo, 1)
+                    control.waitMicros(PwmValue)
+                    pins.digitalWritePin(PinNo, 0)
+                }
+            })
         }
-    }
-    function loop(){
-        control.inBackground(function () {
-            while (true) {
-                while (input.runningTimeMicros() < NextTime) {
-                    basic.pause(2)
-                }
-                NextTime += Interval;
-                switch (PwmMode) {
-                    case pwmMode.servo:
-                        pins.digitalWritePin(PinNo, 1)
-                        control.waitMicros(Math.round(PwmValue * 1000 / 95) + 400)
-                        pins.digitalWritePin(PinNo, 0)
-                        break;
-                    case pwmMode.analog:
-                        pins.digitalWritePin(PinNo, 1)
-                        control.waitMicros(Math.round(PwmValue * 1000 / 95) + 400)
-                        pins.digitalWritePin(PinNo, 0)
-                        break;
-                }
-            }
-        })
     }
 }
--- test.ts
+++ test.ts
@@ -1,3 +1,6 @@
-// tests go here; this will not be compiled when this package is used as a library
-softPWM.initPwm(pwmMode.servo, 20000);
-softPWM.setPwmValue(DigitalPin.P8, 90)
+basic.forever(function () {
+    for(let d=0;d<=180;d+=10){
+        softservo.softServo(DigitalPin.P1, d)
+        basic.pause(1000)
+    }
+})
